ee7b3492179abaf275b842e66d1200202018da5a
soumayg9673 <soumayg9673@palanhaar.in>
2023-02-19 11:59:12 +0530

    [T01-T10] businessapp models and admin panel added

--- businessapp/models.py
+++ businessapp/models.py
@@ -1,3 +1,40 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from userapp import models as UserModel
+
+
+class TimestampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class OrganizationType(models.Model):
+    entity = models.CharField(max_length=30, verbose_name='Entity')
+    document = models.CharField(max_length=30, verbose_name='Document')
+    def __str__(self):
+        return self.entity
+
+
+def upload_to_organization(instance,filename):
+    organizationname = instance.name
+    return f'business/organization/{organizationname}/{filename}'
+
+class Organization(TimestampModel):
+    entity = models.ForeignKey(OrganizationType, on_delete=models.CASCADE, verbose_name='Entity')
+    name = models.CharField(max_length=60, verbose_name='Name')
+    registration = models.CharField(max_length=30, unique=True, verbose_name='Document Number')
+    document = models.FileField(_('Document'), upload_to=upload_to_organization)
+    is_active = models.BooleanField(default=False, verbose_name='Verified')
+    def __str__(self):
+        return self.name
+
+
+class OrganizationEmployee(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, verbose_name='Organization')
+    user = models.ForeignKey(UserModel.User, on_delete=models.CASCADE, verbose_name='Employee')
+    manager = models.BooleanField(default=False, verbose_name='Manager')
+    def __str__(self):
+        return self.organization.name
---
+++ businessapp/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+
+from businessapp import models
+
