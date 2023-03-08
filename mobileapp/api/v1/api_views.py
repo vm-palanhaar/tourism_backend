20b9afb2a191f3965baa62055cc9ff478dbeb57d
soumayg9673 <soumayg9673@palanhaar.in>
2023-03-09 02:07:55 +0530

    [T01-T12] mobile app feedback fix for authenticated users

--- mobileapp/api/v1/api_views.py
+++ mobileapp/api/v1/api_views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from mobileapp import models
 from mobileapp import serializers
@@ -14,14 +15,20 @@ DEV
 failed_response_map = {'error':None}
 response_map = {'data':None}
 
+is_error = 'Something went wrong. Issue reported to Team and your account will be de-activated.'
+
+def error_response(error):
+    failed_response_map['error'] = error
+    return Response(failed_response_map, status=status.HTTP_400_BAD_REQUEST)
+
 
 class MobileAppFeedbackAPIView(generics.CreateAPIView):
     serializer_class = serializers.MobileAppFeedbackSerializer
+    permission_classes = [IsAuthenticated]
 
-    def post(self, request, *args, **kwargs):       
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
-        failed_response_map['error'] = 'Something went wrong!'
-        return Response(failed_response_map, status=status.HTTP_400_BAD_REQUEST)
+        return error_response('Something went wrong!')
